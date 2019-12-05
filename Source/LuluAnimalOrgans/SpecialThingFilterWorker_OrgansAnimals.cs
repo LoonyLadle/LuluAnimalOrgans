9e0e3e1b2c660414cd4db95327be7bee6a425967
Lulu <48167745+LoonyLadle@users.noreply.github.com>
2019-12-04 20:09:22 -0500

    1.0.3 (#3)
    Added stockpile filters to natural body parts for animal or human-like.

---
+++ Source/LuluAnimalOrgans/SpecialThingFilterWorker_OrgansAnimals.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace LoonyLadle.AnimalOrgans
+{
+	public class SpecialThingFilterWorker_OrgansAnimal : SpecialThingFilterWorker
+	{
+		public override bool Matches(Thing t)
+		{
+			return t.TryGetComp<CompOrganOrigin>().originDef.race.Animal;
+		}
+	}
+}
---
+++ Source/LuluAnimalOrgans/SpecialThingFilterWorker_OrgansHumanlike.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace LoonyLadle.AnimalOrgans
+{
+	public class SpecialThingFilterWorker_OrgansHumanlike : SpecialThingFilterWorker
+	{
+		public override bool Matches(Thing t)
+		{
+			return t.TryGetComp<CompOrganOrigin>().originDef.race.Humanlike;
+		}
+	}
+}
